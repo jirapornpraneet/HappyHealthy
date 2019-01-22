ad7b5d501c2ba2c2c5e9d21e41ba7a5442550781
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-01-22 15:55:58 +0700

    edit show results last name

--- HappyHealthy/ViewController.swift
+++ HappyHealthy/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         print("RealmTest\(String(describing: Realm.Configuration.defaultConfiguration.fileURL))")
 
         let user = UserResource()
-        user.name = "jiraporn3"
+        user.name = "jiraporn"
         user.gender  = "female"
 
         try? realm?.write {
@@ -26,5 +26,6 @@ class ViewController: UIViewController {
 
         let results = realm?.objects(UserResource.self)
         print(results![0].name!)
+        print(results!.last!.name!)
     }
 }
