f9456667d87a646ce31cb70b34d8b59c38b98544
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-01-24 13:45:39 +0700

    add class ShowDiabetesVIewController

---
+++ ViewController/Diabetes/ShowDiabetesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShowDiabetesViewController.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 24/1/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+
+class ShowDiabetesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
