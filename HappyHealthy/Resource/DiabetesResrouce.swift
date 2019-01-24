73603ae889c265e87a33765d9826a7653efed7df
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-01-24 11:30:52 +0700

    add class DiabetesResrouce

---
+++ HappyHealthy/Resource/DiabetesResrouce.swift
@@ -0,0 +1,17 @@
+//
+//  DiabetesResrouce.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 24/1/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class DiabetesResrouce: Object {
+    @objc dynamic var dateTime: String?
+    @objc dynamic var costSugar = 0
+    @objc dynamic var diabetesLevel: String?
+    @objc dynamic var diabetesStatus: String?
+}
