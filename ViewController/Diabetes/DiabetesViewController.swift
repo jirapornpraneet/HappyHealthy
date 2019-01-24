8db6455dc73da099d3445babaca59eeaf1800fdb
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-01-24 11:32:15 +0700

    add folder and class DiabetesViewController

---
+++ ViewController/Diabetes/DiabetesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiabetesViewController.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 24/1/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+
+class DiabetesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
