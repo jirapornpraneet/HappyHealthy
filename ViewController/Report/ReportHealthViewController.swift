18b604a65e1d971d5b13373228ab69196199b8be
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-02-05 11:41:57 +0700

    WIP : add class ReportHealthViewController

---
+++ ViewController/Report/ReportHealthViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ReportHealthViewController.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 5/2/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+
+class ReportHealthViewController: UIViewController {
+
+//    @IBOutlet var costSugarLabel: UILabel!
+//    @IBOutlet var dateDiabetesLabel: UILabel!
+//    @IBOutlet var levelDiabetesLabel: UILabel!
+//    @IBOutlet var costGFRLabel: UILabel!
+//    @IBOutlet var dateKidneyLabel: UILabel!
+//    @IBOutlet var levelKidneyLabel: UILabel!
+//    @IBOutlet weak var datePressureLabel: UILabel!
+//    @IBOutlet weak var costPressureTopLabel: UILabel!
+//    @IBOutlet weak var costPressureDownLabel: UILabel!
+//    @IBOutlet var levelPresureLabel: UILabel!
+//    @IBOutlet weak var costHeartLabel: UILabel!
+//    @IBOutlet var levelHeartLabel: UILabel!
+//    @IBOutlet weak var alertDiabetesImage: UIImageView!
+//    @IBOutlet weak var alertKidneyImage: UIImageView!
+//    @IBOutlet weak var alertPressureImage: UIImageView!
+//    @IBOutlet weak var alertHeartImage: UIImageView!
+//    @IBOutlet weak var picker: AAPickerView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
