d09b606be66a4c39ac6cfadc60d013eb92dc6ead
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-02-07 15:14:38 +0700

    add class FoodViewController

---
+++ ViewController/Food/FoodViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FoodViewController.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 7/2/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class FoodViewController: UIViewController {
+    
+    var realm = try? Realm()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
