efd40de14aad16efafd4b6b4429dbb98d9471ce0
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-02-12 11:25:51 +0700

    add class ExerciseHistoryResource

---
+++ HappyHealthy/Resource/ExerciseHistoryResource.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseHistoryResource.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 12/2/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ExerciseHistoryResource: Object {
+    @objc dynamic var historyExerciseId = 0
+    @objc dynamic var historyExerciseDate: String?
+    @objc dynamic var exerciseId = 0
+    @objc dynamic var exerciseTotalDuration: Double = 0.0
+    @objc dynamic var sumExerciseCalories: Double = 0.0
+}
