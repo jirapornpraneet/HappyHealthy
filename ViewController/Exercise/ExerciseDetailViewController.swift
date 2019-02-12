824b26929c6994f02804eb96bf696bd40dfeda7e
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2019-02-12 11:03:43 +0700

    add class ExerciseDetailViewController

---
+++ ViewController/Exercise/ExerciseDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ExerciseDetailViewController.swift
+//  HappyHealthy
+//
+//  Created by Jiraporn Praneet on 12/2/2562 BE.
+//  Copyright © 2562 jirapornpraneetHappyHealthy. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ExerciseDetailViewController: UIViewController {
+
+    var exerciseDetailResource: ExerciseResource!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
